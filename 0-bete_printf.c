33a9e7541385e3ff99b49d9468653a442dda3ee9
Benjamin Appiah <ben.appiah451@gmail.com>
2023-11-13 14:44:28 -0800

    first file created, please check for errors and try fix them, we need to get this one working at befor sleeping, please

---
+++ 0-bete_printf.c
@@ -0,0 +1,57 @@
+#include "main.h"
+#include <stdio.h>
+
+/**_printf - handles format specifiers, a, s, and %
+ * @format: First parameter
+ * Return: Success
+ */
+
+int _printf(const char *format, ...)
+{
+	va_list args;
+	char *i, j;
+	int char_count;
+
+	if (format == NULL)
+	{
+		return(1);
+		exit(98);
+	}
+	va_start(args, format);
+	while (*format != '\0')
+	{
+		if (*format != '%' & *(format + 1) != '\0')
+		{
+			_putchar(*format);
+			char_count++;
+		}
+		else
+		{
+			format++;
+			if (*format == 'c')
+			{	
+				i = va_arg(args, int);
+				_putchar(i);
+				char_count++;
+			}
+			else if (*format == 's')
+			{
+				j = va_arg(args, char *);
+				while (*s != '/0')
+				{
+					_putchar(*s);
+					char_count++;
+					j++;
+				}
+			}
+			else if (*format == '%')
+			{
+				_putchar('%');
+				char_count++;
+			}
+		}
+			format++;
+	}
+	va_end(args);
+	return (char_count);
+}
