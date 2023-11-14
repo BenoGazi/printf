660ae9e8d8e0fb275f54abd9f14dc988549888d2
--replace-all <okiriotega@gmail.com>
2023-11-14 07:15:04 -0800

    a function that handles conversion specifiers d and i

--- 1-bete_int_handlers.c
+++ 1-bete_int_handlers.c
@@ -1,28 +1,36 @@
 #include "main.h"
 
 /**
- * handle_int_i - prints an integer value
+ * handle_int_d_i - prints an integer value
  * @args: integer argument
  * Return: number of characters printed
  */
 
-int handle_int_i(va_list args)
+int handle_int_d_i(va_list args)
 {
 	int n = va_arg(args, int);
-	unsigned int i;
+	unsigned int d, i;
 	int int_count = 0;
 
 	i = (n < 0) ? -n : n;
+	d = (n < 0) ? -n : n;
+
 	if (n < 0)
 	{
 		_putchar('-');
 		int_count++;
 	}
-	if (n / 10 != 0)
+	do
+	{
+		_putchar((i % 10) + 0);
+		int_count++;
+	}
+	while (i != 0);
+
+	if (d / 10 != 0)
 	{
 		int_count += handle_int_i(args);
 	}
-	_putchar((i % 10) + '0');
 	int_count++;
 	return (int_count);
 }
