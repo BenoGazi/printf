d453c3e2c576b71cf93e9aef9ec946d543b868c8
Benjamin Appiah <ben.appiah451@gmail.com>
2023-11-15 08:07:08 -0800

    still working but errors keep coming

--- 0-bete_printf.c
+++ 0-bete_printf.c
@@ -14,31 +14,36 @@ int _printf(const char *format, ...)
 
 	unique_id ID[ID_COUNT] = {
 		{"%c", handle_c}, {"%s", handle_s}, {"%d", handle_int_d_i},
-		{"%%", handle_per_cent}, {"%i", handle_int_d_i}, {"%u", handle_u}, {"%b", handle_b}
+		{"%%", handle_per_cent}, {"%i", handle_int_d_i},
+		{"%u", handle_u}, {"%b", handle_b}
 	};
 
-	if (format == NULL)
+	if (!format)
 		return (-1);
 	va_start(args, format);
-	while (*format != '\0')
+	while (*format)
 	{
 		if (*format != '%' || *(format + 1) == '\0')
 		{
 			_putchar(*format);
 			char_count++;
-			format++;
 		}
 		else
 		{
-			i = 0;
-			while (i < ID_COUNT && ID[i].key[1] == format[1])
+			for (i = 0; i < ID_COUNT && ID[i].key[1] != format[1]; i++)
+				;
+			if (i < ID_COUNT)
 			{
-				i++;
-				char_count += (i == ID_COUNT) ?
-				(_putchar(*format++), 1) : ID[i].id(args);
+				char_count += ID[i].id(args);
+				format += 2;
 			}
-			format++;
+			else
+			{
+				_putchar(*format++);
+				continue;
+			}
 		}
+		format++;
 	}
 	va_end(args);
 	return (char_count);
