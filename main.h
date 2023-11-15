483783a5e6b2cdc04e2c637877dd5b9de4e02c90
--replace-all <okiriotega@gmail.com>
2023-11-15 12:54:56 -0800

    prototypes for o_x_X

--- main.h
+++ main.h
@@ -21,5 +21,7 @@ int handle_c(va_list args);
 int handle_s(va_list args);
 int handle_per_cent(void);
 int handle_b(va_list args);
+int handle_o(va_list args);
+int handle_x_X(va_list args, int uppercase);
 
 #endif /* MAIN_H */
